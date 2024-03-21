fc6ce44bb6806d59419280405c10518cd014ce76
clubd <danenrmar@gmail.com>
2024-03-21 16:42:04 -0400

    initilizing files for loading bar creation

---
+++ src/components/LoadingBar/LoadingBar.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { useState } from "react";
+
+function LoadingBar() => {
+    const [fill, setFill] = useState 
+
+    return (
+        <>
+
+        </>
+        )
+}
