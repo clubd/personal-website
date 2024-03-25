8918566dba9b00349b503afd22f2fb299209c1b0
clubd <danenrmar@gmail.com>
2024-03-25 16:12:23 -0400

    create laoding component, add state for loading bar to begin and fill, add loading speed

--- src/components/LoadingBar/LoadingBar.js
+++ src/components/LoadingBar/LoadingBar.js
@@ -1,12 +1,26 @@
-import React from "react";
-import { useState } from "react";
+import React, { useEffect, useState } from "react";
+
 
 function LoadingBar() => {
-    const [fill, setFill] = useState 
+    const [fill, setFill] = useState(0);
+    const [loading, isLoading] = useState(false);
+
+    useEffect(() => {
+        const interval = setInterval(() => {
+            setFill((previousFill) => {
+                if (previousFill === 100) {
+                    clearInterval(interval);
+                }
+                return previousFill + 1;
+            });
+        }, 20);
+    },[]);
 
     return (
         <>
-
+        <div className="loading-container"
         </>
         )
 }
+
+export default LoadingBar
